890f821a5878eca036a0e6af40b8b66104d19c51
Raja-chandrawanshi <rajachandrawanshi96@gmail.com>
2023-03-17 03:11:03 +0530

    form validation

---
+++ Form_Validation/src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import GoogleForm1 from "./assets/Component/state3";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<GoogleForm1 />);
