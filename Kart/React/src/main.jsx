5969c135579c81a63d48de288e51b0e963b40a2a
Raja-chandrawanshi <rajachandrawanshi96@gmail.com>
2023-03-22 20:29:51 +0530

    kart

---
+++ Kart/React/src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import Greets from "./ParentKart";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Greets />);
